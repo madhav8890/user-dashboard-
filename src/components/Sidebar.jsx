b2b510698e3b18eca87dff1e2b9ff809f90a6be2
Madhav Prajapati <madhavku248@gmail.com>
2023-03-18 18:15:08 +0530

    SideBar compleated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,14 +7,39 @@ import {links} from "../data/data/dummy"
   
 const Sidebar = () => {
   const activeMenu = true;
+  const activeLink = 'flex item-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-black text-md m-2';
+  const normalLink = 'flex item-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-black text-md text-gary-700 drak:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+  
   return (
     <div className='ml-3 h-screen md:ovelflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
       <div className='flex justify-between item-center'>
         <Link to="/" onClick={()=>{}} className='item-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
-          <SiShopware/> <span>shoppy</span>
+          <SiShopware className='mt-1'/> <span>shoppy</span>
         </Link>
+        <TooltipComponent content="Menu" position='BottomCenter'>
+          <button type='button' onClick={()=>{}} className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block '>
+            <MdOutlineCancel/>
+          </button>
+        </TooltipComponent>
 
+      </div>
+      <div className='mt-10'>
+        { links.map((item) => (
+          <div key={item.title}>
+            <p className='text-gray-400 m-3 mt-4 uppercase'>
+            {item.title}
+            </p>
+            {item.links.map((Link) => (
+              <NavLink to={`/${Link.name}`} key={Link.name} onClick={()=>{}} className={({isActive}) => isActive? activeLink: normalLink}>
+                {Link.icon}
+                <span className='capitalize'>
+                    {Link.name}
+                </span>
+              </NavLink>
+            ))}
+          </div>
+        ))}
       </div>
       </>)}
 
